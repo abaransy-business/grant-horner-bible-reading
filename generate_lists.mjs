8dfc4abb3615da89c99ff1fd31aa78647f361aca
abaransy-business <155489746+abaransy-business@users.noreply.github.com>
2026-03-02 12:39:34 -0800

    Add files via upload

---
+++ generate_lists.mjs
@@ -0,0 +1,99 @@
+import fs from "fs/promises";
+import path from "node:path";
+const listsStartAndFinish = [
+  ["40_Matthew", "43_John"],
+  ["01_Genesis", "05_Deuteronomy"],
+  ["45_Romans", "58_Hebrews"],
+  ["52_I_Thessalonians", "66_Revelation_of_John"],
+  ["18_Job", "22_Song_of_Solomon"],
+  ["19_Psalms"],
+  ["20_Proverbs"],
+  ["06_Joshua", "17_Esther"],
+  ["23_Isaiah", "39_Malachi"],
+  ["44_Acts"],
+];
+
+const fullLists = [];
+
+for (let i = 0; i < 10; i++) {
+  fullLists.push([]);
+}
+
+// console.log(fullLists);
+
+const allBooks = await fs.readdir("./by_chapter");
+
+async function readFilesInOrder(dirPath) {
+  const filePaths = await fs.readdir(dirPath);
+
+  filePaths.sort();
+
+  const fileContents = [];
+
+  for (const filePath of filePaths) {
+    const fullPath = path.join(dirPath, filePath);
+    const fileContent = await fs.readFile(fullPath, "utf-8");
+
+    fileContents.push(fileContent);
+  }
+
+  return fileContents;
+}
+
+try {
+  let currentBookIndex = 0;
+
+  for (let i = 0; i < listsStartAndFinish.length; i++) {
+    const currentListStartAndFinish = listsStartAndFinish[i];
+
+    const startingBookWithinList = currentListStartAndFinish[0];
+
+    currentBookIndex = allBooks.findIndex(
+      (book) => book === startingBookWithinList,
+    );
+
+    const endingBookWithinList = currentListStartAndFinish[1];
+
+    if (!endingBookWithinList) {
+      const currentBook = allBooks[currentBookIndex];
+      const chaptersForCurrentBook = await readFilesInOrder(
+        `./by_chapter/${currentBook}`,
+      );
+
+      fullLists[i].push(chaptersForCurrentBook);
+      currentBookIndex++;
+    } else {
+      while (true) {
+        const currentBook = allBooks[currentBookIndex];
+
+        if (
+          (i === 4 && currentBook === "19_Psalms") ||
+          currentBook === "20_Proverbs"
+        ) {
+          currentBookIndex++;
+          continue;
+        }
+
+        const chaptersForCurrentBook = await readFilesInOrder(
+          `./by_chapter/${currentBook}`,
+        );
+
+        fullLists[i].push(chaptersForCurrentBook);
+
+        if (currentBook === endingBookWithinList) {
+          break;
+        }
+
+        currentBookIndex++;
+      }
+    }
+  }
+  console.log(fullLists);
+
+  fs.writeFile(
+    "./fullLists.js",
+    `const fullLists = ${JSON.stringify(fullLists)}`,
+  );
+} catch (err) {
+  console.error(err);
+}
